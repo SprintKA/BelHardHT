c166d6a2b6dd9c7984dfbe2cc207b3dae9f750cd
Androsenko Kirill <kirllandrosenko@gmail.com>
2024-10-28 23:18:40 +0300

    Add strs

--- lesson_3/main.py
+++ lesson_3/main.py
@@ -2,3 +2,18 @@ firstname = 'Kirill'
 lastname = 'Androsenko'
 age = 19
 print (f"Hello, my name is {firstname} {lastname}, I'm {age} years old")
+
+str_3_enter = 'REVERS'
+print(str_3_enter[2])
+print('==============')
+str_last_enter = 'REVERS'
+print(str_last_enter[-2])
+print('==============')
+str_first_5_enter = 'REVERS'
+print(str_first_5_enter[:5])
+print('==============')
+str_without_last_2_enter = 'REVERS'
+print(str_without_last_2_enter[:4])
+print('==============')
+some_str = "Hello world"
+print(some_str.replace('world' ,'Kirill'))
